10a9767ae6cb69a38fa7086fd3c67e6fdf3f59f4
lucaaas <lucasoliveira2306@gmail.com>
2023-02-15 18:07:24 -0300

    Criada estrutura base para o frontend. #1[]

---
+++ frontend/src/routes/main/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import Card from '@smui/card';
+</script>
+
+<h1> Hello world </h1>
+
+<div class="card-container">
+    <Card padded>A simple padded card.</Card>
+</div>
+
+
+<style lang="scss">
+    @use "@material/card";
+</style>
