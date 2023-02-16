9dd39fd4587736a379c426d0ea4f610c39145b64
lucaaas <lucasoliveira2306@gmail.com>
2023-02-16 15:43:42 -0300

    Adicionado pagina principal para manter a lista de TODO. #1

--- frontend/src/routes/main/+page.svelte
+++ frontend/src/routes/main/+page.svelte
@@ -1,14 +1,44 @@
 <script lang="ts">
-    import Card from '@smui/card';
+    import Icon from '@smui/textfield/icon';
+    import CardComponent from '../../lib/cardComponent/CardComponent.svelte';
+    import TextFieldComponent from '../../lib/textField/TextFieldComponent.svelte';
+
+    let items: Array<string> = [];
+    let newItem: string = '';
+
+    /**
+     * Adds an item to the items list
+     * @param item item to be added
+     */
+    function addItem(item: string): void {
+      if (item !== '') {
+        resetNewField();
+
+        items.push(item);
+        items = items;
+      }
+    }
+
+    function resetNewField() {
+      newItem = '';
+    }
 </script>
 
 <h1> Hello world </h1>
 
-<div class="card-container">
-    <Card padded>A simple padded card.</Card>
-</div>
+<CardComponent>
+  <span slot="title"> Lista TO-DO </span>
+  <span slot="content">
+    {#each items as item, index }
+      <TextFieldComponent label="" value={item}>
+        <Icon class="material-icons" slot="leading"> add </Icon>
+      </TextFieldComponent>
+    {/each}
 
+    <TextFieldComponent label="Novo item" onKeyEnter={addItem} bind:value={newItem}>
+      <Icon class="material-icons" slot="leading"> add </Icon>
+    </TextFieldComponent>
+  </span>
+</CardComponent>
 
-<style lang="scss">
-    @use "@material/card";
-</style>
+{items}
