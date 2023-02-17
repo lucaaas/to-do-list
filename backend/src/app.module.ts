2a9d123bb06e695c22a273c571cbfd7ecfe9520f
lucaaas <lucasoliveira2306@gmail.com>
2023-02-17 17:07:50 -0300

    Corrigido erros de conexao com o banco.

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,29 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ItemModule } from './item/item.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ItemModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'db',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'myapp',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+
+export class AppModule {
+  constructor(private dataSource: DataSource) {
+  }
+}
