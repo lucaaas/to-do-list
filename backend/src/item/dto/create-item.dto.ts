add7aadf497551433b9430222d868c403822ad55
lucaaas <lucasoliveira2306@gmail.com>
2023-02-19 12:23:53 -0300

    Adicionado documentacao das funcoes do backend. #1

--- backend/src/item/dto/create-item.dto.ts
+++ backend/src/item/dto/create-item.dto.ts
@@ -1,3 +1,6 @@
+/**
+ * Data transfer object for creating a new item.
+ */
 export class CreateItemDto {
   public id: number;
   public description: string;
--- backend/src/item/entities/item.entity.ts
+++ backend/src/item/entities/item.entity.ts
@@ -1,5 +1,12 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
+/**
+ * Represents an Item entity in the database.
+ *
+ * @property id The unique identifier of the item in the database.
+ * @property description The description of the item.
+ * @property done A boolean indicating if the item is done or not.
+ */
 @Entity()
 export class Item {
   @PrimaryGeneratedColumn()
