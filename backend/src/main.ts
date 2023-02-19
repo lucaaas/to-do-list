31c41d3c7d1e987206c5a069258f8021d9d1bda9
lucaaas <lucasoliveira2306@gmail.com>
2023-02-18 23:00:54 -0300

    Alterado main para permitir CORS. #1

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as express from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+    next();
+  });
+
   await app.listen(3000);
 }
+
 bootstrap();
