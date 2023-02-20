c01ec4ba06399b9b1babe4911c99be278259461a
lucaaas <lucasoliveira2306@gmail.com>
2023-02-20 14:26:13 -0300

    Adicionado componente de checkbox. #2

---
+++ frontend/src/lib/checkbox/CheckboxComponent.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import Checkbox from '@smui/checkbox';
+
+  export let checked: boolean = false;
+  export let onToggle: (value: boolean) => void;
+
+  function toggle(): void {
+    onToggle(!checked);
+  }
+</script>
+
+<div class="checkbox">
+  <Checkbox bind:checked on:click={toggle} />
+</div>
+
+<style lang="scss">
+  @use "styles";
+  .checkbox {
+    align-self: center;
+  }
+</style>
