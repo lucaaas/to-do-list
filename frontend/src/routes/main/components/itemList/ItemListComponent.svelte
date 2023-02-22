df2414a8db5c607424028a1cb29525524f326078
lucaaas <lucasoliveira2306@gmail.com>
2023-02-22 19:25:58 -0300

    Adicionado feature de edicao do item. #3

--- frontend/src/routes/main/components/itemList/ItemListComponent.svelte
+++ frontend/src/routes/main/components/itemList/ItemListComponent.svelte
@@ -3,8 +3,9 @@
   @component
   A Svelte component that renders a list of items, each one with a checkbox and a text field.
 
-  @param items - An array of items to be displayed.
-  @param onToggle - A function that will be called when the checkbox value changes.
+  @prop items An array of items to be displayed.
+  @prop onToggle A function that will be called when the checkbox value changes.
+  @prop onEditItem A function that will be called when the description of an Item changes.
 -->
 <script lang="ts">
   import CheckboxComponent from '../../../../lib/checkbox/CheckboxComponent.svelte';
@@ -13,10 +14,21 @@
 
   export let items: Array<ItemModel>;
   export let onToggle: (item: ItemModel, value: boolean) => void;
+  export let onEditItem: (item: ItemModel, value: string) => void;
+
+  function leaveInput(item: ItemModel, value: string) {
+    if (item.description !== value) {
+      onEditItem(item, value);
+    }
+  }
+
 </script>
 
 {#each items as item, index}
-  <TextFieldComponent label="" value={item.description} onKeyEnter={() => {}}>
+  <TextFieldComponent label=""
+                      value={item.description}
+                      onKeyEnter={(value) => {leaveInput(item, value)}}
+                      onFocusOut={(value) => {leaveInput(item, value)}}>
     <CheckboxComponent slot="leading" checked={item.done}
                        onToggle={(value) => onToggle(item, value)} />
   </TextFieldComponent>
